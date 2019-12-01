2bd9ef9f07af2a0d373e7cf6762b3a62b6bdf067
carlosjcuevasmpucmm <carlosjcuevasmpucmm@gmail.com>
2019-12-01 02:26:07 -0400

    Tested book creating. no audio or text yet

--- app/api/models/book.js
+++ app/api/models/book.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const OrderSchema = new Schema({
 
  
-  Title: {
+  title: {
   type: String,
   required: true,
   },
 
-  Author: {
+  author: {
     type: String,
     required:true,
   },
@@ -25,29 +25,8 @@ const OrderSchema = new Schema({
     required: true
   },
 
-  //Un arreglo de String, que de no recibir entrada correcta, efectivo por defecto
- payment: {
-    type: String,
-    enum: ['tarjeta','efectivo'],
-    default: 'efectivo',
-    required: true,
-    trim: true
-},
-
-payed:{    
-    type: Boolean,
-    default: false,
-    required: true
-},
-
+  
 });
 
-// Una orden solo tiene 1 usuario
-OrderSchema.index({
-  guid: 1,
-},
-{
-unique:true
-})
 
 module.exports = mongoose.model('BookModel', OrderSchema)
