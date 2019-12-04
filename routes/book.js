739d82c44b8f783fb1a3e3106f0ef420e2a085a9
carlosjcuevasmpucmm <carlosjcuevasmpucmm@gmail.com>
2019-12-04 05:32:42 -0400

    Implmentando tts. Error filepath del tts desde server...

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const bookController = require('../app/api/controllers/book');
+const multer = require('multer');
+const { memoryStorage } = require('multer');
+const TTS = require('../app/AWS/TTS');
 
 
-router.post('/', bookController.create);
+
+const m = multer({
+    storage: memoryStorage(),
+    limits: {
+      fileSize: 5 * 1024 * 1024 
+    }
+  });
+
+router.post('/', bookController.create, m.single("file"),TTS.uploadAudio);
 router.get('/', bookController.getAll);
 router.get('/:bookId', bookController.getById);
 router.delete('/:bookId', bookController.deleteById);
